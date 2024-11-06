d2eca9551fb7b5744b82217b8448aa7c5f9946db
Alexandru Nastase <nastase.alexandru100@gmail.com>
2024-11-06 18:24:41 +0000

    1_DS_18_Trees

---
+++ 1_DS_18_Trees/Trees.java
@@ -0,0 +1,29 @@
+public class Trees {
+    public static void main(String[] args) {
+        // Tree - A non-linear DS with Nodes organised in a hierarchy
+        // Made up of Nodes and Edges
+
+        // ... A
+        // .../.\
+        // ..B...C
+        // ./.\
+        // D...E
+
+        // Terminology:
+
+        // Root Node - top of the tree. No incoming Edges
+        // Leaf Node - bottom of the tree. No outgoing Edges
+        // Branch/Inner Nodes - in the middle. Have both incoming and outgoing Edges
+        // Parent - Node with outgoing Edges
+        // Child - Node with incoming Edge. Nodes can be both parents and children
+        // Siblings - any Nodes sharing the same parent
+        // Subtree - a smaller Tree held within a larger Tree
+        // Size of Tree - number of Nodes
+        // Depth of Tree - number of Nodes on the longest path Root -> Leaf
+        // Depth of Node - number of Nodes on the path from Root (Root starts at 0)
+        // Height of Tree - same as Depth
+        // Height of Node - number of Nodes from that to the deepest Leaf (start at 0)
+        // Diameter of Tree - number of Nodes on the longest path between any two Leafs
+
+    }
+}
